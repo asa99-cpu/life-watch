cf79c14360c29b437e5099de42c01841649665c8
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2024-12-25 12:21:16 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,15 +1,11 @@
 import streamlit as st
 
 def sidebar_inputs():
-    st.sidebar.title("Life Watch Settings")
+    st.sidebar.header("Life Watch Settings")
     expected_lifespan = st.sidebar.number_input(
-        "Enter your expected lifespan (in years):",
-        min_value=1,
-        value=80,
+        "Expected Lifespan (Years)", min_value=1, max_value=120, value=80, step=1
     )
     current_age = st.sidebar.number_input(
-        "Enter your current age:",
-        min_value=0,
-        value=25,
+        "Current Age (Years)", min_value=0, max_value=120, value=25, step=1
     )
     return expected_lifespan, current_age
